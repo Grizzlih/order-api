e4ecde2f6e299d973b220920055f6c6cf074b016
grizzlih <ed.gizbreht@gmail.com>
2019-08-27 20:22:26 +0300

    fixed the test of users

--- test/routes/user.spec.ts
+++ test/routes/user.spec.ts
@@ -40,6 +40,21 @@ describe('userRoute', () => {
                 expect(res.body.username).to.be.equal(user.username);
             });
     });
+    it('should return the user updated on the step before', async () => {
+        return chai
+            .request(app)
+            .get(`/users/${user.username}`)
+            .then(res => {
+                expect(res.status).to.be.equal(200);
+                expect(res.body.username).to.be.equal(user.username);
+                expect(res.body.firstName).to.be.equal(user.firstName);
+                expect(res.body.lastName).to.be.equal(user.lastName);
+                expect(res.body.email).to.be.equal(user.email);
+                expect(res.body.password).to.be.equal(user.password);
+                expect(res.body.phone).to.be.equal(user.phone);
+                expect(res.body.userStatus).to.be.equal(user.userStatus);
+            });
+    });
     it('should updated the user John', async () => {
         user.username = 'John Updated';
         user.firstName = 'John Updated';
@@ -72,7 +87,7 @@ describe('userRoute', () => {
                 expect(res.body.userStatus).to.be.equal(user.userStatus);
             });
     });
-    it('should return 404 becaise the user does not exist', async () => {
+    it('should return 404 because the user does not exist', async () => {
         user.firstName = 'Mary Jane';
 
         return chai
