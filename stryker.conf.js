078ef6473f77492903fd5a06029ddb216188396e
grizzlih <ed.gizbreht@gmail.com>
2019-08-28 18:15:35 +0300

    update stryker config

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,21 +1,20 @@
 module.exports = function(config) {
-  config.set({
-          testRunner: 'mocha',
-          mochaOptions: {
-              files: ['test/**/*.spec.ts'],
-              opts: './test/mocha.opts',
-              ui: 'bdd',
-              require: ['ts-node/register', 'source-map-support/register'],
-              asyncOnly: false,
-          },
-          mutator: 'typescript',
-          transpilers: [],
-          reporters: ['html', 'progress', 'dashboard'],
-          packageManager: 'npm',
-          testFramework: 'mocha',
-          coverageAnalysis: 'off',
-          tsconfigFile: 'tsconfig.json',
-          mutate: ['src/**/*.ts'],
-      })
-  }
-  
+    config.set({
+        testRunner: 'mocha',
+        mochaOptions: {
+            files: ['test/**/*.spec.ts'],
+            opts: './test/mocha.opts',
+            ui: 'bdd',
+            require: ['ts-node/register', 'source-map-support/register'],
+            asyncOnly: false,
+        },
+        mutator: 'typescript',
+        transpilers: [],
+        reporters: ['html', 'progress', 'dashboard'],
+        packageManager: 'npm',
+        testFramework: 'mocha',
+        coverageAnalysis: 'off',
+        tsconfigFile: 'tsconfig.json',
+        mutate: ['src/**/*.ts'],
+    });
+}
